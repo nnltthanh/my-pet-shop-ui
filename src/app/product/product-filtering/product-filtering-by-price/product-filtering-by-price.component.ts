770ae998c3f1072bbf517896c756debb833fda2e
Lam Thanh <thanhreika2002@gmail.com>
2024-08-17 22:16:44 +0700

    basic product page

--- src/app/product/product-filtering/product-filtering-by-price/product-filtering-by-price.component.ts
+++ src/app/product/product-filtering/product-filtering-by-price/product-filtering-by-price.component.ts
@@ -1,39 +1,18 @@
-import { LabelType, NgxSliderModule, Options } from '@angular-slider/ngx-slider';
-import { CurrencyPipe } from '@angular/common';
+import { NgxSliderModule } from '@angular-slider/ngx-slider';
+import { CurrencyPipe, NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-filtering-by-price',
   standalone: true,
-  imports: [NgxSliderModule, CurrencyPipe],
+  imports: [NgxSliderModule, CurrencyPipe, FormsModule, NgIf, NgClass],
   templateUrl: './product-filtering-by-price.component.html',
   styleUrl: './product-filtering-by-price.component.scss'
 })
 export class ProductFilteringByPriceComponent {
 
-  minValue: number = 0;
-  maxValue: number = 30000000;
-  options: Options = {
-    floor: 0,
-    ceil: 30000000,
-    step: 200000,
-    translate: (value: number, label: LabelType): string => {
-      switch (label) {
-        case LabelType.Low:
-          return `<b>Tối thiểu: </b>${this.formatVNDCurrency(value)} \u20ab`;
-        case LabelType.High:
-          return `<b>Tối đa: </b>${this.formatVNDCurrency(value)} \u20ab`;
-        default:
-          return ``;
-      }
-    }
-  };
-
-  private formatVNDCurrency(value: number): string {
-    if (value < 1_000_000) {
-      return `${value}`;
-    }
-    return `${parseFloat(value.toString()) / 1_000_000} triệu`;
-  }
+  minValue?: number;
+  maxValue?: number;
 
 }
--- src/app/product/product-list-display/product-list-display.component.ts
+++ src/app/product/product-list-display/product-list-display.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { Product } from '../product.model';
+import { ProductListCardComponent } from './product-list-card/product-list-card.component';
 
 @Component({
   selector: 'app-product-list-display',
   standalone: true,
-  imports: [],
+  imports: [ProductListCardComponent, NgSelectModule, FormsModule],
   templateUrl: './product-list-display.component.html',
-  styleUrl: './product-list-display.component.scss'
+  styleUrl: './product-list-display.component.scss',
 })
-export class ProductListDisplayComponent {
+export class ProductListDisplayComponent implements OnInit {
+  products?: Product[] = [];
+  sortingOptions = [
+    { id: 1, option: 'Tải lên gần nhất' },
+    { id: 2, option: 'Đánh giá từ cao tới thấp' },
+    { id: 3, option: 'Giá từ thấp tới cao' },
+    { id: 4, option: 'Giá từ cao tới thấp' },
+  ];
+  selectedSortingOption = 1;
 
+  ngOnInit(): void {
+    let product: Product = {
+      id: 1,
+      name: '',
+      engName: '',
+      price: 0,
+      description: '',
+      imageData: '',
+      updatedAt: new Date(),
+      rate: 0,
+    };
+    for (let index = 0; index < 30; index++) {
+      this.products?.push(product);
+    }
+  }
 }
